35a28101898c9afdfc7d08d838184143401e8985
myusername <myemail>
2019-06-13 00:49:23 +0300

    Доработал адаптив, проработал модальное окно, адаптив модального окна

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,7 +3,22 @@ $(document).ready(function() {
   var modal = $('#modal');
   var close = $('#close');
   var card = $('.card__link')
+  var butt = $('.imgs')
 
+
+//   butt.on('click', function(){	// Событие клика на маленькое изображение
+//     var img = $(this);	// Получаем изображение, на которое кликнули
+//   var src = img.attr("src"); // Достаем из этого изображения путь до картинки
+//   $("body").append(<div class='popup'><div class='popup_bg'></div><img src="src" class='popup_img'/></div>); 
+//   $(".popup")(800); // Медленно выводим изображение
+//   $(".popup_bg").click(function(){	// Событие клика на затемненный фон	   
+//     $(".popup")(800);	// Медленно убираем всплывающее окно
+//     setTimeout(function() {	// Выставляем таймер
+//       $(".popup").remove(); // Удаляем разметку всплывающего окна
+//     }, 800);
+//   });
+// });
+ 
   button.on('click', function() {
     modal.addClass('modal_active');
   });
